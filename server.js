13d49391e1568c85d9a4ac74c56e54aa8e8f56d0
guofengxian <guofengxian@xin.com>
2017-05-13 16:38:13 +0800

    test

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-// server.js
-var http = require('http');
+var express = require('express')
+var app = express()
 
-http.createServer(function (request, response) {
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
 
-    // 发送 HTTP 头部 
-    // HTTP 状态值: 200 : OK
-    // 内容类型: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-
-    // 发送响应数据 "Hello World"
-    response.end('Hello World\n');
-}).listen(8888);
-
-// 终端打印如下信息
-console.log('Server running at http://127.0.0.1:8888/');
+app.listen(process.env.PORT || 5050)
