b4e487b962e63735e00e8f268befe1853f4b2f1a
guofengxian <guofengxian@xin.com>
2017-05-13 18:49:19 +0800

    body

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var sha1 = require("sha1");
-var app = express()
+var bodyParser = require('body-parser')
+
+var app = express();
+app.use(bodyParser.json())
+
+
 
 app.get('/', function (req, res) {
 	var q = req.query; 
@@ -11,7 +16,7 @@ app.get('/', function (req, res) {
 	var appsecret = '3b1f610929071dbababe53a46a2c4cf0';
 	var str = [appsecret, timestamp, nonce].sort().join('');  
     var sha = sha1(str); 
-    console.log(sha); 
+    // console.log(sha); 
     if (req.method == 'GET') {  
 
         if (sha == signature) {  
@@ -27,6 +32,12 @@ app.get('/', function (req, res) {
     //     next();  
     // }  
   // res.send('Hello World')
+})
+app.post('/',function(req,res){
+	console.log(req.body);
+	// res.send(req.body);
+	// res.redirect("/users")//重定向告诉客户端向另外一个地址发请求；
+	//res.redirect("back")//*****从哪来回哪去。
 })
 
 app.listen(process.env.PORT || 5050)
