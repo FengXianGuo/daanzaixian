c518d4609149088988454d4d494a6c69454ee37f
guofengxian <guofengxian@xin.com>
2017-05-13 18:02:26 +0800

    sha1

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
-var express = require('express')
+var express = require('express');
+var sha1 = require("sha1");
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+	var q = req.query; 
+	var signature = q.signature;
+	var timestamp = q.timestamp;
+	var echostr = q.echostr;
+	var nonce = q.nonce;
+	var appsecret = '3b1f610929071dbababe53a46a2c4cf0';
+	var str = [appsecret, timestamp, nonce].sort().join('');  
+    var sha = sha1(str); 
+    console.log(sha); 
+    if (req.method == 'GET') {  
+
+        if (sha == signature) {  
+            res.send(echostr+'')  
+        }else{  
+            res.send('err');  
+        }  
+    }  
+    // else if(req.method == 'POST'){  
+    //     if (sha != signature) {  
+    //         return;  
+    //     }  
+    //     next();  
+    // }  
+  // res.send('Hello World')
 })
 
 app.listen(process.env.PORT || 5050)
