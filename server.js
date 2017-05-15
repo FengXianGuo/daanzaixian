9e6ee60ae6cf186084657a63144d8186fbd089c6
guofengxian <guofengxian@xin.com>
2017-05-15 11:35:31 +0800

    ssss

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ var bodyParser = require('body-parser')
 
 var app = express();
 app.use(bodyParser.json())
+// app.use(bodyParser.xml({
+//   limit: '1MB',   // Reject payload bigger than 1 MB
+//   xmlParseOptions: {
+//     normalize: true,     // Trim whitespace inside text nodes
+//     normalizeTags: true, // Transform tags to lowercase
+//     explicitArray: false // Only put nodes in array if >1
+//   }
+// }));
+
 
 
 
@@ -34,43 +43,6 @@ app.get('/', function (req, res) {
     // }  
   // res.send('Hello World')
 })
-app.get('/wechat',function(req,res){
-	// var signature = req.query.signature;
-	// var timestamp = req.query.timestamp;
-	// var echostr   = req.query.echostr;
-	// var nonce     = req.query.nonce;
-	// var oriArray = new Array();
-	// oriArray[0] = nonce;
-	// oriArray[1] = timestamp;
-	// oriArray[2] = token;
-	// var result = oriArray.sort().join();
-	// var sha = sha1(result);
-	// console.log("sha="+sha);
-	// console.log("signture="+signature);
-	//  if(sha == signature){
-	//  	res.send(echostr + '');
-	//  //验证成功
-	//  } else {
-	//  //验证失败
-	//  	res.send('err');
-	//  }
-	var token="weixin";
-    var signature = req.query.signature;
-    var nonce = req.query.nonce;
-    var timestamp = req.query.timestamp;
-    var echostr = req.query.echostr;
-    var str = [token, timestamp, nonce].sort().join('');
-    var sha = sha1(str);
-    console.log("sha="+sha);
-	console.log("signture="+signature);
-    //将你自己生成的signature和服务器传过来的signature来进行比较，如果成功则验证通过。
-    if (sha === signature) {
-        res.send(echostr + '');
-    }
-    else {
-        res.send('wrong');
-    }
-})
 app.post('/',function(req,res){
 	// console.log(req.body);
 	var obj = {
@@ -88,6 +60,58 @@ app.post('/',function(req,res){
 
 	res.json(obj);
 })
+
+//接入微信初史验签
+app.get('/wechat',function(req,res){
+	var token="weixin";
+    var signature = req.query.signature;
+    var nonce = req.query.nonce;
+    var timestamp = req.query.timestamp;
+    var echostr = req.query.echostr;
+    var str = [token, timestamp, nonce].sort().join('');
+    var sha = sha1(str);
+    console.log("sha="+sha);
+	console.log("signture="+signature);
+    //将你自己生成的signature和服务器传过来的signature来进行比较，如果成功则验证通过。
+    if (sha === signature) {
+        res.send(echostr + '');
+    }
+    else {
+        res.send('wrong');
+    }
+})
+app.post('/wechat',function(req,res){
+    var _da;
+    req.on("data",function(data){
+        /*微信服务器传过来的是xml格式的，是buffer类型，因为js本身只有字符串数据类型，所以需要通过toString把xml转换为字符串*/
+        _da = data.toString("utf-8");
+
+    });
+    req.on("end",function(){
+        //console.log("end");
+        var ToUserName = getXMLNodeValue('ToUserName',_da);
+        var FromUserName = getXMLNodeValue('FromUserName',_da);
+        var CreateTime = getXMLNodeValue('CreateTime',_da);
+        var MsgType = getXMLNodeValue('MsgType',_da);
+        var Content = getXMLNodeValue('Content',_da);
+        var MsgId = getXMLNodeValue('MsgId',_da);
+        console.log(ToUserName);
+        console.log(FromUserName);
+        console.log(CreateTime);
+        console.log(MsgType);
+        console.log(Content);
+        console.log(MsgId);
+        var xml = '<xml><ToUserName>'+FromUserName+'</ToUserName><FromUserName>'+ToUserName+'</FromUserName><CreateTime>'+CreateTime+'</CreateTime><MsgType>'+MsgType+'</MsgType><Content>'+Content+'</Content></xml>';
+        res.send(xml);
+    });
+
+	function getXMLNodeValue(node_name,xml){
+	    var tmp = xml.split("<"+node_name+">");
+	    var _tmp = tmp[1].split("</"+node_name+">");
+	    return _tmp[0];
+	}
+})
+
 
 app.listen(process.env.PORT || 5050)
 app.on("error",function(e){
