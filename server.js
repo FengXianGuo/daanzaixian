0ab5d4de702eb0cf08ae2f64100bb5b286a6af15
guofengxian <guofengxian@xin.com>
2017-05-15 11:08:06 +0800

    ssss

--- server.js
+++ server.js
@@ -34,6 +34,26 @@ app.get('/', function (req, res) {
     // }  
   // res.send('Hello World')
 })
+app.get('/wechat',function(req,res){
+	var token="weixin";
+	var signature = req.query.signature;
+	var timestamp = req.query.timestamp;
+	var echostr   = req.query.echostr;
+	var nonce     = req.query.nonce;
+	var oriArray = new Array();
+	oriArray[0] = nonce;
+	oriArray[1] = timestamp;
+	oriArray[2] = token;
+	var result = oriArray.sort().join();
+	var sha = sha1(result);
+	 if(sha == signature){
+	 	res.send(echostr + '');
+	 //验证成功
+	 } else {
+	 //验证失败
+	 	res.send('err');
+	 }
+})
 app.post('/',function(req,res){
 	// console.log(req.body);
 	var obj = {
