1e3cfe59e51143ba58917fed7c429137c2154274
guofengxian <guofengxian@xin.com>
2017-05-16 17:13:43 +0800

    fixed

--- routes/answer.js
+++ routes/answer.js
@@ -4,39 +4,47 @@ let router = new Router()
 
 
 router.post('/wechat', async(ctx,next)=>{
-	// console.log(123);
-	var token="weixin";
-	var q = ctx.query;
-    var signature = q.signature;
-    var nonce = q.nonce;
-    var timestamp = q.timestamp;
-    var echostr = q.echostr;
-    var str = [token, timestamp, nonce].sort().join('');
-    var sha = sha1(str);
-    //将你自己生成的signature和服务器传过来的signature来进行比较，如果成功则验证通过。
-    if (sha === signature) {
-        ctx.body = echostr + '';
-    }
-    else {
-        ctx.body = 'wrong';
+	var _da;
+    req.on("data",function(data){
+        /*微信服务器传过来的是xml格式的，是buffer类型，因为js本身只有字符串数据类型，所以需要通过toString把xml转换为字符串*/
+        _da = data.toString("utf-8");
+
+    });
+    req.on("end",function(){
+        //console.log("end");
+        var ToUserName = getXMLNodeValue('ToUserName',_da);
+        var FromUserName = getXMLNodeValue('FromUserName',_da);
+        var CreateTime = getXMLNodeValue('CreateTime',_da);
+        var MsgType = getXMLNodeValue('MsgType',_da);
+        var Content = getXMLNodeValue('Content',_da);
+        var MsgId = getXMLNodeValue('MsgId',_da);
+        console.log(ToUserName);
+        console.log(FromUserName);
+        console.log(CreateTime);
+        console.log(MsgType);
+        console.log(Content);
+        console.log(MsgId);
+        var xml = '<xml><ToUserName>'+FromUserName+'</ToUserName><FromUserName>'+ToUserName+'</FromUserName><CreateTime>'+CreateTime+'</CreateTime><MsgType>'+MsgType+'</MsgType><Content>'+Content+'</Content></xml>';
+        res.send(xml);
+    });
+
+    function getXMLNodeValue(node_name,xml){
+        var tmp = xml.split("<"+node_name+">");
+        var _tmp = tmp[1].split("</"+node_name+">");
+        return _tmp[0];
     }
 }); // responds to "/users"
 router.post('/weibo', async (ctx,next)=>{
-	var q = ctx.query; 
-	var signature = q.signature;
-	var timestamp = q.timestamp;
-	var echostr = q.echostr;
-	var nonce = q.nonce;
-	var appsecret = '3b1f610929071dbababe53a46a2c4cf0';
-	var str = [appsecret, timestamp, nonce].sort().join('');  
-    var sha = sha1(str); 
-    // console.log(sha); 
-    if (ctx.method == 'GET') {  
-        if (sha == signature) {  
-            ctx.body = echostr+'';  
-        }else{  
-            ctx.body = 'err';  
-        }  
-    }
+	var obj = {
+        "result":true,
+        "sender_id":req.body.receiver_id,
+        "receiver_id":req.body.sender_id,
+        "type":"text",
+        "data":encodeURI(JSON.stringify({
+            "text":req.body.text
+        }))
+    };
+
+    res.json(obj);
 }); // responds to "/users/:id"
 module.exports = router;
